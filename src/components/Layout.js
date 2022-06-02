dcf9513d02af4ce6e0669302ce331dff78da14ea
Parkseonho <tjsgh2103@gmail.com>
2022-06-02 21:24:59 +0900

    레이아웃 꾸미기

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,23 +1,39 @@
 import * as React from "react";
 import { Link } from "gatsby";
+import {
+  container,
+  heading,
+  navLinks,
+  navLinkItem,
+  navLinkText,
+  articleBottom,
+} from "./layout.module.css";
+
 const Layout = ({ pageTitle, children }) => {
   return (
-    <div>
+    <div className={container}>
       <title>{pageTitle}</title>
       <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
+        <ul className={navLinks}>
+          <li className={navLinkItem}>
+            <Link className={navLinkText} to="/">
+              Home
+            </Link>
           </li>
-          <li>
-            <Link to="/Ggosom">Ggosom</Link>
+          <li className={navLinkItem}>
+            <Link className={navLinkText} to="/Ggosom">
+              Ggosom
+            </Link>
           </li>
         </ul>
       </nav>
       <main>
-        <h1>{pageTitle}</h1>
+        <h1 className={heading}>{pageTitle}</h1>
         {children}
       </main>
+      <article>
+        <p className={articleBottom}>Copyright All Rights</p>
+      </article>
     </div>
   );
 };
