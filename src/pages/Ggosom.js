6bae85b89116e1290eb59044125bdfde5b553f97
Parkseonho <tjsgh2103@gmail.com>
2022-06-02 21:04:01 +0900

    새로운 페이지 추가

---
+++ src/pages/Ggosom.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Ggosom = () => {
+  return (
+    <main>
+      <title>Ggosom Page</title>
+      <h1> 진짜 귀여움 </h1>
+    </main>
+  );
+};
+
+export default Ggosom;
